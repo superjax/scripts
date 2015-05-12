7c76bd8e373d8871d69d5d87364d8a39cffb1d0f
David Wheeler <david.wheeler@byu.edu>
2015-05-12 08:57:51 -0600

    Adding some more plots

--- matlab/path_with_covariance.m
+++ matlab/path_with_covariance.m
@@ -22,9 +22,9 @@ nIters = 20;
 [Xa,Pa] = import_pose_graph('~/rosbag/CENTENNIAL.bag');
 
 % Alter data
-jumps = find(diag(Xa(:,1:2)*Xa(:,1:2).') > 1.5);
-Xa(jumps,1:2) = repmat([0 0],length(jumps),1);
-Pa(jumps,1:2) = 500*Pa(jumps,1:2);
+%jumps = find(diag(Xa(:,1:2)*Xa(:,1:2).') > 1.5);
+%Xa(jumps,1:2) = repmat([0 0],length(jumps),1);
+%Pa(jumps,1:2) = 500*Pa(jumps,1:2);
 %P(50:60,6) = 500*P(50:60,6);
 
 % Set location/attitude of initial node
@@ -34,7 +34,7 @@ p0 = [0 0 0 0 0 0];
 axis([-10  170  -20   100])
 
 % Plot results
-figure(3); clf;
+figure(2); clf;
 axis([-10  170  -20   100])
 plot_centennial_truth
 [handles,labels] = plot_compounded_edges(GT,GP,GT_noise)
@@ -54,7 +54,7 @@ p0 = [0 0 0 0 0 0];
 axis([-10  170  -20   100])
 
 % Plot results
-figure(4); clf;
+figure(1); clf; axis('equal')
 axis([-10  170  -20   100])
 plot_centennial_truth
 plot_compounded_edges(GT,GP,GT_noise)
@@ -66,7 +66,10 @@ nIters = 20;
 % Import data from rosbag
 [Xa,Pa] = import_pose_graph('~/rosbag/03_WSC_2015_02_24.bag');
 
+
 % Alter data
+Pa(125:150,6) = 500*Pa(125:150,6); % Add yaw issue
+Pa(150:160,1) = 500*Pa(150:160,1); % Add yaw issue
 %jumps = find(abs(Xa(:,2)) > 0.4);
 %Xa(jumps,1:2) = repmat([0 0],length(jumps),1);
 %Pa(jumps,1:2) = 500*Pa(jumps,1:2);
@@ -78,9 +81,31 @@ p0 = [0 0 0 0 0 0];
 %axis([-10  170  -20   100])
 
 % Plot results
-figure(3); clf;
+figure(10); clf;
 axis([  -19.4128   44.3546  -17.3552   22.5441])
-%plot_centennial_truth
 [handles,labels] = plot_compounded_edges(GT,GP,GT_noise)
 labels{4} = 'Truth';
 legend([handles h_gps],labels,'Location','SouthEast');
+
+%% TMP Data
+clear all;
+nIters = 20;
+% Import data from rosbag
+[Xa,Pa] = import_pose_graph('~/.ros/estimator.bag');
+
+% Alter data
+%jumps = find(abs(Xa(:,2)) > 0.4);
+%Xa(jumps,1:2) = repmat([0 0],length(jumps),1);
+%Pa(jumps,1:2) = 500*Pa(jumps,1:2);
+
+% Set location/attitude of initial node
+x0 = [0 0 0 0 0 95*pi/180];
+p0 = [0 0 0 0 0 0];
+[GT,GP,GT_noise] = compound_edges(Xa,Pa,nIters,x0,p0);
+%axis([-10  170  -20   100])
+
+% Plot results
+figure(5); clf; axis('equal')
+axis([-10  170  -20   100])
+plot_centennial_truth
+[handles,labels] = plot_compounded_edges(GT,GP,GT_noise)
