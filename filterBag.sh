132bd961c39c2d51fff4bcea40e5ed53e66edfc4
James Jackson <superjax08@gmail.com>
2016-04-29 14:24:23 -0600

    cleaned up installation scripts, added atom, got rid of rand stuff not in the matlab folder

--- filterBag.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $# -eq 0 ]] ; then
-    echo 'enter 1: bagfile input, 2: output bagfile name'
-    exit 0
-fi
-
-rosbag filter $1 $2 "topic in ['/alt_msgs','/current_state','/imu/data','/vo_updates','/mekf/current_state']"
